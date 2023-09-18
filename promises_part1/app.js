207d99f3683c692d8ba2fe2a45ba61cfc942d60d
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2023-09-18 14:53:07 -0500

    second commit

--- promises_part1/app.js
+++ promises_part1/app.js
@@ -1,13 +1,11 @@
 $(function () {
   let baseURL = "https://deckofcardsapi.com/api/deck";
 
-  // 1.
   $.getJSON(`${baseURL}/new/draw/`, function (data) {
     let { suit, value } = data.cards[0];
     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
   });
 
-  // 2.
   $.getJSON(`${baseURL}/new/draw/`, function (data) {
     let firstCard = data.cards[0];
     let deckId = data.deck_id;
@@ -21,7 +19,6 @@ $(function () {
     });
   });
 
-  // 3.
   let deckId = null;
   let $btn = $("button");
   let $cardArea = $("#card-area");
